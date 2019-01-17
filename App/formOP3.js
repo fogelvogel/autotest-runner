69fc25b8a551681ca02631cb6143f1e9c9d16569
Elena Popova <44632346+fogelvogel@users.noreply.github.com>
2019-01-17 09:49:25 +0700

    lab3

--- App/formOP3.js
+++ App/formOP3.js
@@ -1,8 +1,22 @@
-﻿
-let organisation = document.getElementById("comp-input");
+﻿let organisation = document.getElementById("comp-input");
 let part = document.getElementById("parts-input");
 let position = document.getElementById("position-input");
 let button = document.getElementById("add-button");
+let fio = document.getElementById("fio-input");
+
+let products = [];
+let productFathers = [];
+let countProd = 0;
+let units = [];
+let prodNames = [];
+let unitNames = [];
+let unitFathers = [];
+let countUnits = 0;
+let countNames = 0;
+let countUnitNames = 0;
+
+let counter = 0;
+
 organisation.addEventListener("change", () => {
     let OKPOFather = document.querySelector("#okpo");
     OKPOFather.removeChild(document.querySelector("#okpo input"));
@@ -82,18 +96,157 @@ position.addEventListener("change", () => {
             break;
         }
     }
+    fioInput.setAttribute("list","surnames");
+    fioInput.setAttribute("id","fio-input");
     fioFather.appendChild(fioInput);
+});
+fio.addEventListener("change", () => {
+    let posFather = document.querySelector("#pos-father");
+    posFather.removeChild(document.querySelector("#pos-father input"));
+
+    let posInput = document.createElement("input");
+    posInput.setAttribute("type", "text");
+    switch(fio.value) {
+        case "Штейн Карл": {
+            posInput.setAttribute("value", "Директор");
+            break;
+        }
+        case "Зимний Яков Иванович": {
+            posInput.setAttribute("value", "Начальник кладовой");
+            break;
+        }
+        case "Попеску Эмилиэн Петрика": {
+            posInput.setAttribute("value", "Дворник");
+            break;
+        }
+        default: {
+            posInput.setAttribute("value", "");
+            break;
+        }
+    }
+    posInput.setAttribute("list","positions");
+    posInput.setAttribute("id","position-input");
+    posFather.appendChild(posInput);
 });
 
 button.addEventListener("click", () => {
 let table = document.querySelector("#table-to-insert");
 let row = document.createElement("tr");
 let td = [];
-for (let i = 0; i < 7; i++) {
+counter++;
+td[0] = document.createElement("td");
+td[0].append(counter);
+row.appendChild(td[0]);
+for (let i = 1; i < 7; i++) {
     let tmp = document.createElement("input");
     tmp.setAttribute("type", "text");
-    tmp.setAttribute("class", "text");
     td[i] = document.createElement("td");
+
+    switch (i) {
+        case 1: {
+           
+            tmp.addEventListener("change", () => {
+           
+                let currName = tmp.value;
+                let numbers = prodNames.map((current) => {
+                   
+                    switch (current.value) {
+                        case 'Яблоко': return '4532';
+                        case 'Кашка': return '1458';
+                        case 'Жасминовый фреш': return '9568';
+                        case 'Семга': return '4728';
+                        case 'Сыр': return '4235';
+                        case 'Таракан': return '7852';
+                        default: return;
+                    }
+                });
+
+                // let numbers = ["1", "2", "3", "4", "5", "6"];
+
+                for (let j = 0; j < countProd; j++) {
+                    if (currName === prodNames[j].value) {
+                        productFathers[j].removeChild(products[j]);
+                        let newElem = document.createElement("input");
+                        newElem.setAttribute("type", "text");
+                        newElem.setAttribute("class", "code-of-product");
+                        newElem.setAttribute("list", "codes");
+                        newElem.setAttribute("value", numbers[j]);
+    
+                        productFathers[j].appendChild(newElem);
+                        break;
+                    }
+                    
+                }
+
+            });
+            
+            tmp.setAttribute("class", "name-of-product");
+            tmp.setAttribute("list", "goods");
+            prodNames[countNames] = tmp;
+            
+            countNames++;
+            break;
+        }
+        case 2: {
+            productFathers[countProd] = td[i];
+            products[countProd] = tmp;
+            countProd++;
+            tmp.setAttribute("class", "code-of-product");
+            tmp.setAttribute("list", "codes");
+            break;
+        }
+        case 3: {
+           
+            tmp.addEventListener("change", () => {
+                let currName = tmp.value;
+                let numbers = unitNames.map((current) => {
+                   
+                    switch (current.value) {
+                        case 'штука': return '214';
+                        case 'коробка': return '754';
+                        case 'ящик': return '213';
+                        default: return;
+                    }
+                });
+
+                for (let j = 0; j < countUnits; j++) {
+                    if (currName === unitNames[j].value) {
+                        unitFathers[j].removeChild(units[j]);
+                        let newElem = document.createElement("input");
+                        newElem.setAttribute("type", "text");
+                        newElem.setAttribute("class", "code-of-unit");
+                        newElem.setAttribute("list", "unit-codes");
+                        newElem.setAttribute("value", numbers[j]);
+    
+                        unitFathers[j].appendChild(newElem);
+                        break;
+                    }
+                    
+                }
+                });
+            
+                tmp.setAttribute("class", "name-of-unit");
+                tmp.setAttribute("list", "units");
+                unitNames[countUnitNames] = tmp;
+                
+                countUnitNames++;
+                break;
+        }
+        case 4: {
+            unitFathers[countUnits] = td[i];
+            units[countUnits] = tmp;
+            countUnits++;
+            tmp.setAttribute("class", "code-of-unit");
+            tmp.setAttribute("list", "unit-codes");
+            break;
+        }
+        case 5: {
+            tmp.setAttribute("class", "quantity");
+            break;
+        }
+        default: break;
+    }
+    
     td[i].appendChild(tmp);
     row.appendChild(td[i]);
 }
